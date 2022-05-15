d902cbb623a7b61b8fc231715ad438342b1bb9bd
HotTungsten <HotTungsten@gmail.com>
2022-05-15 11:28:50 -0700

    Add book functionality

--- library.js
+++ library.js
@@ -22,8 +22,40 @@ function book(author, title, pages, hasBeenRead) {
 }
 
 function addBookToLibrary() {
-
+    let title = document.getElementById("title-input").value;
+    let author = document.getElementById("author-input").value;
+    let pages = document.getElementById("page-input").value;
+    let readStatus = document.getElementById("read-input").value;
+    
+    if(title !== "" && author !== "" && pages !== "" && readStatus !== ""){
+        myLibrary.push(new book(title, author, pages, readStatus));
+        document.getElementById("form-content").style.display = "none";
+        addBookToPage(new book(title, author, pages, readStatus))
+    }
 }
+
+function addBookToPage(Book){
+    let row = bookTable.insertRow(-1);
+    let authorCell = row.insertCell(0);
+    let titleCell = row.insertCell(1);
+    let pageCell = row.insertCell(2);
+    let readStatusCell = row.insertCell(3);
+
+    authorCell.innerHTML = Book.author;
+    titleCell.innerHTML = Book.title;
+    pageCell.innerHTML = Book.pages;
+    readStatusCell.innerHTML = Book.hasBeenRead;
+}
+
+function openForm() {
+    document.getElementById("form-content").style.display = "block";
+}
+
+const addBookButton = document.getElementById("add-book-button");
+
+addBookButton.addEventListener("click", function() {
+    addBookToLibrary();
+});
 
 function addLibraryToPage(myLibrary) {
     for(let i = 0; i < myLibrary.length; i++){
